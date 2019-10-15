17efb50c7062e119b166c95923eefb875e002d83
kodeky <flaviokryptek@gmail.com>
2019-10-15 00:20:39 -0400

    header update v002

--- header.php
+++ header.php
@@ -1,22 +1,16 @@
-<header>
+<header class="header">
+
+    <div class="header-logo">
+      <img src="img/newlogo.png">
+    
+      <a href="index.php">LOUNGE INK</a>
+    </div>
+
   <nav>
-    <div class="esquerda">
-      <div class="esquerda-item">
-        <img src="img/newlogo.png">
-      </div>
-    </div>
-    <div class="centro">
-      <div class="centro-item">
-        <a class="header-title" href="index.php">TATTOOARIA LOUNGE INK</a>
-      </div>
-    </div>
-    <div class="direita">
-      <div class="direita-link01">
-          <a class="header-link" href="galeria.php?pagina=1&album=Todas">GALERIA</a>
-      </div>
-      <div class="direita-link02">
-          <a class="header-link" href="#footer">CONTATO</a>
-      </div>
-    </div>  
+    <ul class="header-link">
+      <li> <a href="galeria.php?pagina=1&album=Todas">GALERIA</a> </li>
+      <li> <a href="#footer">CONTATO</a> </li>
+    </ul>
   </nav>
+
 </header>
