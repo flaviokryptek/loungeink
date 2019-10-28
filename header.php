4a27f0f82180a6987641bcd349be605ce6c9ca58
kodeky <flaviokryptek@gmail.com>
2019-10-28 10:19:57 -0400

    layout update v281019

--- header.php
+++ header.php
@@ -8,11 +8,15 @@
     <a href="index.php">LOUNGE INK</a>
   </div>
 
-  <nav>
-    <ul class="header-link">
+  <nav class="dropdownmenu">
+    <img src="img/rede-social/menu.png">
+
+    <ul id="hl" class="header-link">
+      <li> <a href="index.php">INICIO</a> </li>
       <li> <a href="galeria.php?pagina=1&album=Todas">GALERIA</a> </li>
       <li> <a href="#footer">CONTATO</a> </li>
     </ul>
+
   </nav>
 
 </header>
