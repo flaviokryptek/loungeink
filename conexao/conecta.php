61d4e73fb30ff1898f4bfe6d9b50c6e026b5cea9
Flávio Lourenço <flaviokryptek@gmail.com>
2020-11-22 22:05:37 +0000

    feat: new dbserver

--- conexao/conecta.php
+++ conexao/conecta.php
@@ -1,7 +1,7 @@
 <?php
-	$host = 'localhost';
-	$usuario = 'flavio';
-	$senha = 'MySQL@2019';
-	$bd = 'lounge_ink';
+	$host = 'https://databases.000webhost.com/';
+	$usuario = 'id11471943_flaviodb';
+	$senha = '<I2l3>';
+	$bd = 'id11471943_lounge_ink';
 	$conn = mysqli_connect($host, $usuario, $senha, $bd) or die ('Erro ao conectar com o banco de dados requisitado');
 ?>
