f88e9c917e283e487fd43c0ad2e78ca17c8a2093
Flavio <flaviokryptek@gmail.com>
2021-01-23 18:46:53 -0400

    break change: refactor

---
+++ admin/data/conecta.php
@@ -0,0 +1,7 @@
+<?php
+	$host = 'https://databases.000webhost.com/';
+	$usuario = 'id11471943_flaviodb';
+	$senha = 'lounge@ink';
+	$bd = 'id11471943_lounge_ink';
+	$conn = mysqli_connect($host, $usuario, $senha, $bd) or die ('Erro ao conectar com o banco de dados requisitado');
+?>
--- conexao/conecta.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$host = 'https://databases.000webhost.com/';
-	$usuario = 'id11471943_flaviodb';
-	$senha = '<I2l3>';
-	$bd = 'id11471943_lounge_ink';
-	$conn = mysqli_connect($host, $usuario, $senha, $bd) or die ('Erro ao conectar com o banco de dados requisitado');
-?>
