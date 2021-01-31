f805103a659c47af8af027b705fa99e099781d09
Flavio <flaviokryptek@gmail.com>
2021-01-31 14:37:06 -0400

    Webscraping attempt

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-heroku aplication
+
+<?php
+  $source = "https://www.worldometers.info/coronavirus/";
+  $content = file_get_contents($source);
+
+  echo $content;
+?>
+
+<script>
+
+  var result = document.evaluate('//*[@id="maincounter-wrap"]/div/span', document , null, XPathResult.STRING_TYPE);
+  
+  document.write("OBITOS: " + result.stringValue);
+  
+</script>
